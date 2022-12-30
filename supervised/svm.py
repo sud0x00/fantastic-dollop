dc9b4ed4163f37565464b3933f65ea1deda3a4af
Sudhanva M <91898207+sud0x00@users.noreply.github.com>
2022-12-31 00:52:06 +0530

    Update svm.py

--- supervised/svm.py
+++ supervised/svm.py
@@ -1,14 +1,21 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
-# Load the data
-X = # input data
-y = # output data
+# Load the iris dataset
+X, y = datasets.load_iris(return_X_y=True)
 
-# Create the model
-model = SVC()
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Fit the model to the data
-model.fit(X, y)
+# Create an SVM classifier with a linear kernel
+clf = SVC(kernel='linear')
 
-# Make predictions on new data
-predictions = model.predict(X_new)
+# Train the classifier using the training data
+clf.fit(X_train, y_train)
+
+# Test the classifier using the test data
+accuracy = clf.score(X_test, y_test)
+
+# Print the accuracy of the classifier
+print('Accuracy:', accuracy)
