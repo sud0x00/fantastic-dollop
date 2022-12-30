7eab3d0e85d9e0bdf532d32d543065eab82bc745
Sudhanva M <91898207+sud0x00@users.noreply.github.com>
2022-12-31 00:48:18 +0530

    Create linear_regression.py

---
+++ supervised/linear_regression.py
@@ -0,0 +1,14 @@
+from sklearn.linear_model import LinearRegression
+
+# Load the data
+X = # input data
+y = # output data
+
+# Create the model
+model = LinearRegression()
+
+# Fit the model to the data
+model.fit(X, y)
+
+# Make predictions on new data
+predictions = model.predict(X_new)
